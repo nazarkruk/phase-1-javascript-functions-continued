096f672fd7ab2c2bedfd629bd032efd1a6d30f2e
Nazar Kruk <nazarkruk@icloud.com>
2022-04-28 16:37:03 -0400

    commit tree 618d9d8c65447391f9ba1b74809fc1744e8d3097

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(funType = 'roller-skate'){
+    return `This Saturday, I want to ${funType}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapper = "*"){
+    return function(arg = "special"){
+        return `You are ${wrapper}${arg}${wrapper}!`
+
+    }
+}
